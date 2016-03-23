b26bcd4efdae2e6dea07e36c4fda05c3e0033772
blavenie <benoit.lavenier@e-is.pro>
2016-03-23 09:59:56 +0100

    preparing config for ucoin.fr with ES

--- www/js/config.js
+++ www/js/config.js
@@ -11,9 +11,9 @@ angular.module("cesium.config", [])
 .constant("APP_CONFIG", {
 	"UCOIN_NODE": "metab.ucoin.fr",
 	"UCOIN_NODE_WS": "metab.ucoin.fr:9201",
-	"UCOIN_NODE_ES": "metab.ucoin.fr:9200",
+	"UCOIN_NODE_ES": "es.ucoin.fr",
 	"VERSION": "0.0.1",
-	"BUILD_DATE": "2016-03-23T08:57:41.603Z"
+	"BUILD_DATE": "2016-03-23T08:59:47.238Z"
 })
 
 ;
