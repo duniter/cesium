2fc01f634c70cf5a07a2a24fa539368d3b0c1d54
blavenie <benoit.lavenier@e-is.pro>
2016-06-08 21:23:31 +0200

    Preapre release 0.1.12

--- www/js/config.js
+++ www/js/config.js
@@ -14,8 +14,8 @@ angular.module("cesium.config", [])
 	"TIMEOUT": 4000,
 	"DEBUG": false,
 	"NATIVE_TRANSITION": false,
-	"VERSION": "0.1.11",
-	"BUILD_DATE": "2016-06-07T11:12:46.157Z"
+	"VERSION": "0.1.12",
+	"BUILD_DATE": "2016-06-08T19:23:09.899Z"
 })
 
 ;
