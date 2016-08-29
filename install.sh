3af20983ab9450b0ea18109395050b8ae32af226
blavenie <benoit.lavenier@e-is.pro>
2016-08-29 21:21:08 +0200

    v0.2.10

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ if [ "_$CESIUM_DIR" = "_" ]; then
 fi
 
 latest_version() {
-  echo "v0.2.9" #lastest
+  echo "v0.2.10" #lastest
 }
 
 api_release_url() {
--- www/js/config.js
+++ www/js/config.js
@@ -9,27 +9,27 @@
 angular.module("cesium.config", [])
 
 .constant("csConfig", {
-	"timeout": 6000,
+	"timeout": 4000,
 	"cacheTimeMs": 60000,
 	"useRelative": true,
 	"timeWarningExpireMembership": 5184000,
 	"timeWarningExpire": 7776000,
-	"useLocalStorage": true,
-	"rememberMe": true,
+	"useLocalStorage": false,
+	"rememberMe": false,
 	"showUDHistory": false,
 	"node": {
-		"host": "192.168.0.28",
-		"port": "9202"
+		"host": "test-net.duniter.fr",
+		"port": "9201"
 	},
 	"plugins": {
 		"es": {
-			"enable": false,
-			"host": "192.168.0.28",
+			"enable": true,
+			"host": "test-net.duniter.fr",
 			"port": "9203"
 		}
 	},
-	"version": "0.2.9",
-	"build": "2016-08-29T16:16:37.610Z",
+	"version": "0.2.10",
+	"build": "2016-08-29T19:20:33.980Z",
 	"newIssueUrl": "https://github.com/duniter/cesium/issues/new?labels=bug"
 })
 
