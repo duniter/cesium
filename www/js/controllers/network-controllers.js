7ea60cb61159a0c4191d5ae4a2fb04d900ae5359
CamilleC <kmille.c.cc@gmail.com>
2017-01-11 14:53:37 +0100

    Separation of node display

---
+++ www/js/controllers/network-controllers.js
@@ -0,0 +1,116 @@
+
+angular.module('cesium.network.controllers', ['cesium.services'])
+
+.config(function($stateProvider) {
+  'ngInject';
+
+  $stateProvider
+
+    .state('app.network_view', {
+      url: "/network/view",
+      views: {
+        'menuContent': {
+          templateUrl: "templates/network/view_network.html",
+          controller: 'NetworkViewCtrl'
+        }
+      },
+    });
+})
+
+.controller('NetworkViewCtrl', NetworkViewController)
+
+.controller('PeerCtrl', PeerController)
+
+;
+
+function NetworkViewController($scope, $q, $translate, $timeout, BMA, UIUtils, csSettings, csCurrency, csNetwork) {
+
+ $scope.loadingPeers = true;
+  $scope.formData = {
+    useRelative: csSettings.data.useRelative
+  };
+  
+  $scope.screen = UIUtils.screen;
+
+  $scope.$on('$ionicView.enter', function(e, state) {
+    $translate(['COMMON.DATE_PATTERN'])
+    .then(function($translations) {
+      $scope.datePattern = $translations['COMMON.DATE_PATTERN'];
+      if (state.stateParams && state.stateParams.name) { // Load by name
+        csCurrency.searchByName(state.stateParams.name)
+        .then(function(currency){
+          $scope.load(currency);
+        });
+      }
+      else {
+        csCurrency.all()
+        .then(function (currencies) {
+          if (currencies && currencies.length > 0) {
+            $scope.load(currencies[0]);
+          }
+
+        })
+        .catch(UIUtils.onError('ERROR.GET_CURRENCY_FAILED'));
+      }      
+    });
+  });
+
+  $scope.$on('$ionicView.beforeLeave', function(){
+    csNetwork.close();
+  });
+
+  $scope.load = function(currency) {
+    $scope.node = !BMA.node.same(currency.peer.host, currency.peer.port) ?
+      BMA.instance(currency.peer.host, currency.peer.port) : BMA;
+
+    if ($scope.loadingPeers){
+      csNetwork.start($scope.node);
+
+      // Catch event on new peers
+      var refreshing = false;
+      csNetwork.api.data.on.changed($scope, function(data){
+        if (!refreshing) {
+          refreshing = true;
+          $timeout(function() { // Timeout avoid to quick updates
+            console.debug("Updating UI Peers");
+            $scope.peers = data.peers;
+            // Update currency params
+
+            $scope.loadingPeers = csNetwork.isBusy();
+            refreshing = false;
+            }, 1100);
+        }
+      });
+      $scope.$on('$destroy', function(){
+        csNetwork.close();
+      });
+    }    
+
+    // Show help tip
+    $scope.showHelpTip();
+  };  
+
+  $scope.refresh = function() {
+    // Network
+    $scope.loadingPeers = true;
+    csNetwork.loadPeers();
+  };
+
+  // Show help tip
+  $scope.showHelpTip = function() {
+    if (!$scope.isLogin()) return;
+    index = csSettings.data.helptip.currency;
+    if (index < 0) return;
+
+    // Create a new scope for the tour controller
+    var helptipScope = $scope.createHelptipScope();
+    if (!helptipScope) return; // could be undefined, if a global tour already is already started
+
+    return helptipScope.startCurrencyTour(index, false)
+      .then(function(endIndex) {
+        helptipScope.$destroy();
+        csSettings.data.helptip.currency = endIndex;
+        csSettings.store();
+      });
+  };
+}
