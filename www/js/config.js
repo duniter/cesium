2b1baca487797e5efff41e83f497b6267f5b8adf
blavenie <benoit.lavenier@e-is.pro>
2017-02-07 16:53:33 +0100

    v0.9.21

--- www/js/config.js
+++ www/js/config.js
@@ -43,8 +43,8 @@ angular.module("cesium.config", [])
 			}
 		}
 	},
-	"version": "0.9.20",
-	"build": "2017-02-04T20:03:22.285Z",
+	"version": "0.9.21",
+	"build": "2017-02-07T15:52:56.674Z",
 	"newIssueUrl": "https://github.com/duniter/cesium/issues/new?labels=bug"
 })
 
