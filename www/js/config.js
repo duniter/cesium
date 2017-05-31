4f91a8e9c164e163c103ec63e8f73c4f2cd5c415
blavenie <benoit.lavenier@e-is.pro>
2017-05-31 16:17:22 +0200

    v0.12.7

--- www/js/config.js
+++ www/js/config.js
@@ -10,59 +10,47 @@ angular.module("cesium.config", [])
 
 .constant("csConfig", {
 	"cacheTimeMs": 60000,
-	"fallbackLanguage": "fr-FR",
-	"defaultLanguage": "fr-FR",
-	"rememberMe": true,
+	"fallbackLanguage": "en",
+	"rememberMe": false,
 	"showUDHistory": false,
-	"timeout": 6000,
+	"timeout": 10000,
 	"timeWarningExpireMembership": 5184000,
 	"timeWarningExpire": 7776000,
+	"logoutIlde": 600,
 	"useLocalStorage": true,
-	"useRelative": true,
-	"expertMode": true,
+	"useRelative": false,
+	"initPhase": false,
+	"expertMode": false,
 	"decimalCount": 2,
+	"httpsMode": false,
 	"helptip": {
-		"enable": false,
-		"installDocUrl": {
-			"fr-FR": "http://www.le-sou.org/devenir-noeud/",
-			"en": "https://github.com/duniter/duniter/blob/master/doc/install-a-node.md"
-		}
+		"enable": true,
+		"installDocUrl": "https://github.com/duniter/duniter/blob/master/doc/install-a-node.md"
 	},
 	"license": {
 		"fr-FR": "license/license_g1-fr-FR.txt",
 		"en": "license/license_g1-en.txt"
 	},
-  "node": {
-    "host": "g1.duniter.fr",
-    "port": "443"
-  },
+	"node": {
+		"host": "g1.duniter.org",
+		"port": "443"
+	},
 	"plugins": {
 		"es": {
-			"enable": false,
+			"enable": true,
 			"askEnable": false,
-			"host": "localhost",
-			"port": 9200,
-			"wsPort": 9400,
+			"host": "g1.data.duniter.fr",
+			"port": "443",
 			"notifications": {
 				"txSent": true,
 				"txReceived": true,
 				"certSent": true,
 				"certReceived": true
 			}
-		},
-		"graph": {
-			"enable": true
-		},
-		"neo4j": {
-			"enable": true
-		},
-    // Activation du plugin RML9
-		"rml9": {
-			"enable": true
 		}
 	},
-	"version": "0.12.6",
-	"build": "2017-05-30T09:15:49.818Z",
+	"version": "0.12.7",
+	"build": "2017-05-31T14:15:50.859Z",
 	"newIssueUrl": "https://github.com/duniter/cesium/issues/new?labels=bug"
 })
 
