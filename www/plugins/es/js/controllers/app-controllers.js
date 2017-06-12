c440fa08b775cd16c3efbb91d9b98c0c12e5fc18
blavenie <benoit.lavenier@e-is.pro>
2017-06-12 16:26:00 +0200

    [fix] remove unused files and code (market place)

--- www/plugins/es/js/controllers/app-controllers.js
+++ www/plugins/es/js/controllers/app-controllers.js
@@ -13,10 +13,6 @@ angular.module('cesium.es.app.controllers', ['ngResource', 'cesium.es.services']
              templateUrl: "plugins/es/templates/menu_extend.html",
              controller: "ESMenuExtendCtrl"
            },
-           'menu-main': {
-             templateUrl: "plugins/es/templates/menu_extend.html",
-             controller: "ESMenuExtendCtrl"
-           },
            'menu-user': {
              templateUrl: "plugins/es/templates/menu_extend.html",
              controller: "ESMenuExtendCtrl"
@@ -79,10 +75,6 @@ function ESMenuExtendController($scope, $state, PluginService, esSettings, UIUti
   'ngInject';
   $scope.extensionPoint = PluginService.extensions.points.current.get();
   $scope.enable = esSettings.isEnable();
-
-  $scope.showMarketLookupView = function() {
-    $state.go(UIUtils.screen.isSmall() ? 'app.market_lookup': 'app.market_lookup_lg');
-  };
 
   $scope.showRegistryLookupView = function() {
     $state.go(UIUtils.screen.isSmall() ? 'app.registry_lookup': 'app.registry_lookup_lg');
