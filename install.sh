d9b6ccdff92240b730ce071eff6945afc07cbe06
blavenie <benoit.lavenier@e-is.pro>
2018-09-03 12:48:04 +0200

    v1.1.5

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ if [ "_$CESIUM_DIR" = "_" ]; then
 fi
 
 latest_version() {
-  echo "v1.1.4" #lastest
+  echo "v1.1.5" #lastest
 }
 
 api_release_url() {
--- www/js/config.js
+++ www/js/config.js
@@ -88,8 +88,8 @@ angular.module("cesium.config", [])
 			"defaultCountry": "France"
 		}
 	},
-	"version": "1.1.4",
-	"build": "2018-08-29T16:57:11.235Z",
+	"version": "1.1.5",
+	"build": "2018-09-03T10:47:07.076Z",
 	"newIssueUrl": "https://git.duniter.org/clients/cesium-grp/cesium/issues/new"
 })
 
