4df0c1fbf0213d7c4afa9be7c53036ec185d2f1e
blavenie <benoit.lavenier@e-is.pro>
2018-09-06 16:04:50 +0200

    [fix] Comment: submit when press Ctrl+Enter

--- www/plugins/es/js/controllers/common-controllers.js
+++ www/plugins/es/js/controllers/common-controllers.js
@@ -214,6 +214,14 @@ function ESCommentsController($scope, $filter, $state, $focus, UIUtils) {
       });
   };
 
+  $scope.onKeypress = function(event) {
+    // If Ctrl + Enter: submit
+    if (event && event.charCode == 10 && event.ctrlKey) {
+      $scope.save();
+      event.preventDefault();
+    }
+  };
+
   $scope.save = function() {
     if (!$scope.formData.message || !$scope.formData.message.length) return;
 
