8476a28c7f393d547fda5cf86487d01f1846b1ee
Bertrand Presles <bpresles@gmail.com>
2019-09-02 09:05:03 +0200

    Using fork of cordova-ios that is wkwebview only

---
+++ typings/cordova-typings.d.ts
@@ -0,0 +1,7 @@
+
+/// <reference path="../.vscode/typings/cordova/cordova.d.ts"/>
+/// <reference path="../.vscode/typings/cordova/plugins/Toast.d.ts"/>
+/// <reference path="../.vscode/typings/cordova-ionic/plugins/keyboard.d.ts"/>
+/// <reference path="../.vscode/typings/ionic/ionic.d.ts"/>
+/// <reference path="../.vscode/typings/angularjs/angular.d.ts"/>
+/// <reference path="../.vscode/typings/jquery/jquery.d.ts"/>
