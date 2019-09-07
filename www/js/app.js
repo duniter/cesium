94a824a02414c840f01e3b1b58fdea781d066190
Bertrand Presles <bpresles@gmail.com>
2019-09-07 14:48:46 +0200

    Found a working solution for overlay status bar, so re-enabled it :)

--- www/js/app.js
+++ www/js/app.js
@@ -138,5 +138,16 @@ angular.module('cesium', ['ionic', 'ionic-material', 'ngMessages', 'pascalprecht
     // Start plugins eager services
     PluginService.start();
 
+    ionicReady().then(function() {
+      if (ionic.Platform.isIOS()) {
+        if(window.StatusBar) {
+          // needed to fix Xcode 9 / iOS 11 issue with blank space at bottom of webview
+          // https://github.com/meteor/meteor/issues/9041
+          StatusBar.overlaysWebView(false);
+          StatusBar.overlaysWebView(true);
+        }
+      }
+    });
+
   })
 ;
