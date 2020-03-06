6ed1f8bd453207f0b41b36e9fa2d762ecb96dc51
Benoit Lavenier <benoit.lavenier@e-is.pro>
2020-03-06 14:48:03 +0100

    v1.5.5

--- www/js/config.js
+++ www/js/config.js
@@ -98,7 +98,7 @@ angular.module("cesium.config", [])
 		}
 	},
 	"version": "1.5.5",
-	"build": "2020-03-06T13:43:25.137Z",
+	"build": "2020-03-06T13:47:02.630Z",
 	"newIssueUrl": "https://git.duniter.org/clients/cesium-grp/cesium/issues/new"
 })
 
