1778e2d20ba88faea04dc9dcb03e67a36788c65d
Benoit Lavenier <benoit.lavenier@e-is.pro>
2023-08-14 18:22:03 +0200

    v1.7.7

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ if [ "_$CESIUM_DIR" = "_" ]; then
 fi
 
 latest_version() {
-  echo "v1.7.6" #lastest
+  echo "v1.7.7" #lastest
 }
 
 api_release_url() {
--- www/js/config.js
+++ www/js/config.js
@@ -29,46 +29,79 @@ angular.module("cesium.config", [])
 		"enable": true,
 		"installDocUrl": {
 			"fr-FR": "https://duniter.fr/wiki/doc/installer/",
-			"en": "https://duniter.org/en/wiki/duniter/install/"
+			"en": "https://duniter.org/wiki/doc/install/"
 		}
 	},
 	"license": {
-		"en": "license/license_g1-en",
-		"fr-FR": "license/license_g1-fr-FR",
-		"es-ES": "license/license_g1-es-ES",
 		"ca": "license/license_g1-ca",
+		"de-DE": "license/license_g1-de-DE",
+		"en": "license/license_g1-en",
+		"en-GB": "license/license_g1-en",
 		"eo-EO": "license/license_g1-eo-EO",
-		"pt-PT": "license/license_g1-pt-PT",
+		"es-ES": "license/license_g1-es-ES",
+		"fr-FR": "license/license_g1-fr-FR",
 		"it-IT": "license/license_g1-it-IT",
-		"de-DE": "license/license_g1-de-DE"
+		"pt-PT": "license/license_g1-pt-PT"
 	},
 	"feed": {
-    "jsonFeed": {
-      "ca": "https://raw.githubusercontent.com/duniter/cesium/master/doc/feed/feed-ca.json",
-      "de-DE": "https://raw.githubusercontent.com/duniter/cesium/master/doc/feed/feed-de-DE.json",
-      "en": "https://raw.githubusercontent.com/duniter/cesium/master/doc/feed/feed-en.json",
-      "en-GB": "https://raw.githubusercontent.com/duniter/cesium/master/doc/feed/feed-en-GB.json",
-      "eo-EO": "https://raw.githubusercontent.com/duniter/cesium/master/doc/feed/feed-eo-EO.json",
-      "es-ES": "https://raw.githubusercontent.com/duniter/cesium/master/doc/feed/feed-es-ES.json",
-      "fr-FR": "https://raw.githubusercontent.com/duniter/cesium/master/doc/feed/feed-fr-FR.json",
-      "it-IT": "https://raw.githubusercontent.com/duniter/cesium/master/doc/feed/feed-it-IT.json",
-      "nl-NL": "https://raw.githubusercontent.com/duniter/cesium/master/doc/feed/feed-nl-NL.json",
-      "pt-PT": "https://raw.githubusercontent.com/duniter/cesium/master/doc/feed/feed-pt-PT.json"
+		"jsonFeed": {
+			"ca": "https://raw.githubusercontent.com/duniter/cesium/master/doc/feed/feed-ca.json",
+			"de-DE": "https://raw.githubusercontent.com/duniter/cesium/master/doc/feed/feed-de-DE.json",
+			"en": "https://raw.githubusercontent.com/duniter/cesium/master/doc/feed/feed-en.json",
+			"en-GB": "https://raw.githubusercontent.com/duniter/cesium/master/doc/feed/feed-en-GB.json",
+			"eo-EO": "https://raw.githubusercontent.com/duniter/cesium/master/doc/feed/feed-eo-EO.json",
+			"es-ES": "https://raw.githubusercontent.com/duniter/cesium/master/doc/feed/feed-es-ES.json",
+			"fr-FR": "https://raw.githubusercontent.com/duniter/cesium/master/doc/feed/feed-fr-FR.json",
+			"it-IT": "https://raw.githubusercontent.com/duniter/cesium/master/doc/feed/feed-it-IT.json",
+			"nl-NL": "https://raw.githubusercontent.com/duniter/cesium/master/doc/feed/feed-nl-NL.json",
+			"pt-PT": "https://raw.githubusercontent.com/duniter/cesium/master/doc/feed/feed-pt-PT.json"
 		},
 		"maxContentLength": 1300,
-    "maxAgeInMonths": -1,
-    "maxCount": 3
+		"maxAgeInMonths": 3,
+		"maxCount": 3
 	},
 	"fallbackNodes": [
-    {
-      "host": "g1.e-is.pro",
-      "port": 443
-    }
+		{
+			"host": "g1.e-is.pro",
+			"port": 443
+		},
+		{
+			"host": "vit.fdn.org",
+			"port": 443
+		},
+		{
+			"host": "g1.cgeek.fr",
+			"port": 443
+		},
+		{
+			"host": "g1.mithril.re",
+			"port": 443
+		},
+		{
+			"host": "g1.duniter.org",
+			"port": 443
+		},
+		{
+			"host": "g1.le-sou.org",
+			"port": 443
+		}
 	],
 	"developers": [
 		{
 			"name": "Benoit Lavenier",
 			"pubkey": "38MEAZN68Pz1DTvT3tqgxx4yQP6snJCQhPqEFxbDk4aE"
+		},
+		{
+			"name": "Cédric Moreau",
+			"pubkey": "2ny7YAdmzReQxAayyJZsyVYwYhVyax2thKcGknmQy5nQ"
+		},
+		{
+			"name": "Kapis",
+			"pubkey": "24jaf8XhYZyDyUb7hMcy5qsanaHBC11AwPefcCQRBQNA"
+		},
+		{
+			"name": "Matograine",
+			"pubkey": "CmFKubyqbmJWbhyH2eEPVSSs4H4NeXGDfrETzEnRFtPd"
 		}
 	],
 	"plugins": {
@@ -113,8 +146,8 @@ angular.module("cesium.config", [])
 			"defaultCountry": "France"
 		}
 	},
-	"version": "1.7.6",
-	"build": "2023-08-02T14:54:32.661Z",
+	"version": "1.7.7",
+	"build": "2023-08-14T16:19:15.798Z",
 	"newIssueUrl": "https://git.duniter.org/clients/cesium-grp/cesium/issues/new"
 })
 
