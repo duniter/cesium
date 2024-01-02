9b47e4aaaa8bb61d42444f919c234a4d218ae32d
Benoit Lavenier <benoit.lavenier@e-is.pro>
2024-01-02 12:34:58 +0100

    v1.7.12

--- www/js/config.js
+++ www/js/config.js
@@ -147,8 +147,8 @@ angular.module("cesium.config", [])
 			"defaultCountry": "France"
 		}
 	},
-	"version": "1.7.11",
-	"build": "2024-01-02T10:24:48.754Z",
+	"version": "1.7.12",
+	"build": "2024-01-02T11:33:49.941Z",
 	"newIssueUrl": "https://git.duniter.org/clients/cesium-grp/cesium/issues/new"
 })
 
